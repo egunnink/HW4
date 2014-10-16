f5797e21637f6f34fd2e354ce9dbc811bf369a5f
egunnink <ezekiel-gunnink@uiowa.edu>
2014-10-16 01:18:28 -0300

    finished all HW4 parts

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -48,41 +48,64 @@ end
 
 
 # Add a declarative step here for populating the DB with movies.
-
 Given /the following movies have been added to RottenPotatoes:/ do |movies_table|
   movies_table.hashes.each do |movie|
-    # Each returned movie will be a hash representing one row of the movies_table
-    # The keys will be the table headers and the values will be the row contents.
-    # You should arrange to add that movie to the database here.
-    # You can add the entries directly to the databasse with ActiveRecord methodsQ
+    # use the same method used in 'seeds.rb'
 	Movie.create!(movie)
   end
 end
 
-When /^I have opted to see movies rated: "(.*?)"$/ do |arg1|
-  # HINT: use String#split to split up the rating_list, then
-  # iterate over the ratings and check/uncheck the ratings
-  # using the appropriate Capybara command(s)
-  rating_list = arg1.gsub(/,/,'').split(' ')
+# filtering steps
+When /^I have opted to see movies rated: "(.*?)"$/ do |ratings|
+  # remove the comma and split by space
+  rating_list = ratings.gsub(/,/,'').split(' ')
   Movie.all_ratings.each do |rating|
+    # make id string
   	find_str = sprintf("input[id$='ratings_%s']", rating)
+    # only check it if it is in our list
   	find(:css, find_str).set(rating_list.include?(rating))
   end
-  click_button 'Refresh'
+  # refresh the list
+  click_button('Refresh')
 end
 
-Then /^I should see only movies rated "(.*?)"$/ do |arg1|
-  rating_list = arg1.gsub(/,/,'').split(' ')
+Then /^I should see only movies rated "(.*?)"$/ do |ratings|
+  # remove the comma and split by space
+  rating_list = ratings.gsub(/,/,'').split(' ')
+  # don't use 'should' it is depricated
+  # the second child is the rating
   all('#movies tr > td:nth-child(2)').each do |rating|
-	assert(rating_list.include?(rating.text), "#{rating.text} is not rated correctly")
+    # check that the rating is in the list
+	assert(rating_list.include?(rating.text), "The movie with rating #{rating.text} should be included in the ratings: #{rating_list}")
   end
 end
 
 Then /^I should see all of the movies$/ do
+  # we seed 10 movies in the background
   expected_num_movies = 10
+  # don't use 'should' it is depricated
   num_movies = all('#movies tr').count
-  assert(num_movies == expected_num_movies + 1, "Expected #{expected_num_movies} got #{num_movies}")# add one for header
+  # don't forget to add 1 for the header
+  assert(num_movies == expected_num_movies + 1, "Expected #{expected_num_movies} got #{num_movies}")
 end
 
+# sorting steps
+When /^I sort the movies alphabetically$/ do
+  # click the title header
+  click_link('title_header')
+end
 
+When /^I sort the movies by release date$/ do
+  # click the release data header
+  click_link('release_date_header')
+end
 
+Then /^I should see "(.*?)" before "(.*?)"$/ do |before_name, after_name|
+  # the 1st child is the name
+  all('#movies tr > td:nth-child(1)').each do |name|
+     # stop if we find the name that is supposed to come first
+     break if name.text == before_name
+     # if we run into the second name first then error
+     assert(name.text != after_name, "#{after_name} came before #{before_name}");
+  end
+end
